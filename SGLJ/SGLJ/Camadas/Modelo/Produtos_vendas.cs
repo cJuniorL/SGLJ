1695db97053d93e6543aa6107fe0fa5a8717994e
AddamCauê <addamcaue@hotmail.com>
2016-06-21 22:35:20 -0300

    Modelo produtos_vendas

---
+++ SGLJ/SGLJ/Camadas/Modelo/Produtos_vendas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGLJ.Camadas.Modelo
+{
+    public class Produtos_vendas
+    {
+        public int idProdutos { get; set; }
+        public int idVendas { get; set; }
+    }
+}
