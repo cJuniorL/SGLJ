3858a797232df1bc22d0d6b0c931f162bded3486
Carlos <junior_rossini9@hotmail.com>
2016-06-21 23:09:57 -0300

    Vendendores

---
+++ SGLJ/SGLJ/Camadas/Modelo/Vendedores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGLJ.Camadas.Modelo
+{
+    public class Vendedores
+    {
+        public int id { get; set; }
+        public int idCidade { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string celular { get; set; }
+        public string endereco { get; set; }
+        public string rg { get; set; }
+        public string cpf { get; set; }
+        public float salario { get; set; }
+    }
+}
