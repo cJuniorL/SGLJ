e7837505e331fc94cfac34278107293870ef9356
AddamCauê <addamcaue@hotmail.com>
2016-06-21 22:43:40 -0300

    mudança clientes e produtos_vendas

--- SGLJ/SGLJ/Camadas/Modelo/Clientes.cs
+++ SGLJ/SGLJ/Camadas/Modelo/Clientes.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SGLJ.Camadas.Modelo
 {
-    class Clientes
+    public class Clientes
     {
+        public int id { get; set; }
+        public int idCidade { get; set; }
+        public int telefone { get; set; }
+        public int celular { get; set; }
+        public string endereco { get; set; }
+        public int cpf { get; set; }
+        public string email { get; set; }
     }
 }
