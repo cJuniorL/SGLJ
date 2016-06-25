7910debc037545b8da4e1bcccb9506abe0bcfa0b
AddamCauê <addamcaue@hotmail.com>
2016-06-25 00:11:19 -0300

    BLL e DAL de Tipo_Produtos

--- SGLJ/SGLJ/Camadas/BLL/Tipo_Produtos.cs
+++ SGLJ/SGLJ/Camadas/BLL/Tipo_Produtos.cs
@@ -8,5 +8,38 @@ namespace SGLJ.Camadas.BLL
 {
     class Tipo_Produtos
     {
+        public List<Modelo.Tipo_Produtos> Select()
+        {
+            DAL.Tipo_Produtos dalCli = new DAL.Tipo_Produtos();
+
+            return dalCli.Select();
+        }
+
+        public List<Modelo.Tipo_Produtos> SelectById(int id)
+        {
+            DAL.Tipo_Produtos dalTipoProdutos = new DAL.Tipo_Produtos();
+
+            return dalTipoProdutos.SelectById(id);
+        }
+
+        public void Insert(Modelo.Tipo_Produtos tipoProdutos)
+        {
+            DAL.Tipo_Produtos dalTipoProdutos = new DAL.Tipo_Produtos();
+            //
+            dalTipoProdutos.Insert(tipoProdutos);
+        }
+
+        public void Update(Modelo.Tipo_Produtos tipoProdutos)
+        {
+            DAL.Tipo_Produtos dalTipoProdutos = new DAL.Tipo_Produtos();
+            dalTipoProdutos.Update(tipoProdutos);
+        }
+
+        public void Delete(Modelo.Tipo_Produtos tipoProdutos)
+        {
+            DAL.Tipo_Produtos dalTipoProdutos = new DAL.Tipo_Produtos();
+
+            dalTipoProdutos.Delete(tipoProdutos);
+        }
     }
 }
--- SGLJ/SGLJ/Camadas/DAL/Tipo_Produtos.cs
+++ SGLJ/SGLJ/Camadas/DAL/Tipo_Produtos.cs
@@ -11,40 +11,132 @@ namespace SGLJ.Camadas.DAL
     class Tipo_Produtos
     {
 
-        //private string strCon = Conexao.getConexao();
+        private string strCon = Conexao.getConexao();
 
-        //public List<Modelo.Tipo_Produtos> Select()
-        //{
-        //    List<Modelo.Tipo_Produtos> lstTipoProdutos = new List<Modelo.Tipo_Produtos>();
-        //    SqlConnection conexao = new SqlConnection(strCon);
-        //    string sql = "Select * from Tipo_Produtos";
-        //    SqlCommand cmd = new SqlCommand(sql, conexao);
-        //    conexao.Open();
-        //    try
-        //    {
-        //        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-        //        while (reader.Read())
-        //        {
-        //            MODEL.Cliente cliente = new MODEL.Cliente();
-        //            cliente.id = Convert.ToInt32(reader[0].ToString());
-        //            cliente.nome = reader["nome"].ToString();
-        //            cliente.endereco = reader["endereco"].ToString();
-        //            cliente.cidade = reader["cidade"].ToString();
-        //            cliente.estado = reader["estado"].ToString();
-        //            cliente.aniversario = Convert.ToDateTime(reader["aniversario"].ToString());
-        //            lstCliente.Add(cliente);
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("Deu erro na Seleção de Clientes...");
-        //    }
-        //    finally
-        //    {
-        //        conexao.Close();
-        //    }
+        public List<Modelo.Tipo_Produtos> Select()
+        {
+            List<Modelo.Tipo_Produtos> lstTipoProdutos = new List<Modelo.Tipo_Produtos>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Select * from Tipo_Produtos";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            conexao.Open();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    Modelo.Tipo_Produtos tipoProdutos = new Modelo.Tipo_Produtos();
+                    tipoProdutos.id = Convert.ToInt32(reader[0].ToString());
+                    tipoProdutos.descr = reader["descr"].ToString();
+                    lstTipoProdutos.Add(tipoProdutos);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na Seleção de Tipo_Produtos...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
 
-        //    return lstCliente;
-        //}
+            return lstTipoProdutos;
+        }
+
+        public List<Modelo.Tipo_Produtos> SelectById(int id)
+        {
+            List<Modelo.Tipo_Produtos> lstTipoProdutos = new List<Modelo.Tipo_Produtos>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Select * from Tipo_Produtos where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", id);
+            conexao.Open();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    Modelo.Tipo_Produtos tipoProdutos = new Modelo.Tipo_Produtos();
+                    tipoProdutos.id = Convert.ToInt32(reader[0].ToString());
+                    tipoProdutos.descr = reader["descr"].ToString();
+                    lstTipoProdutos.Add(tipoProdutos);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na Seleção de Tipo_Produtos por ID...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return lstTipoProdutos;
+        }
+
+        public void Insert(Modelo.Tipo_Produtos tipoProdutos)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Insert into Tipo_Produtos values (@descr);";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@descr", tipoProdutos.descr);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na inserção de Tipo_Produtos...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Update(Modelo.Tipo_Produtos tipoProdutos)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Update Tipo_Produtos set descr=@descr, where id=@id";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", tipoProdutos.id);
+            cmd.Parameters.AddWithValue("@descr", tipoProdutos.descr);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na atualização de Tipo_Produtos");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Delete(Modelo.Tipo_Produtos tipoProdutos)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Delete from Tipo_Produtos where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", tipoProdutos.id);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na Remoção de Tipo_Produtos");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
     }
 }
