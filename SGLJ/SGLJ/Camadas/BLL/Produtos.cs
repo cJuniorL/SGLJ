7efc8937c9fe7e5b2d4934af88bc1f48b2d48739
AddamCauê <addamcaue@hotmail.com>
2016-06-25 03:09:36 -0300

    Produtos BLL e DAL

--- SGLJ/SGLJ/Camadas/BLL/Produtos.cs
+++ SGLJ/SGLJ/Camadas/BLL/Produtos.cs
@@ -8,5 +8,34 @@ namespace SGLJ.Camadas.BLL
 {
     class Produtos
     {
+        public List<Modelo.Produtos> Select()
+        {
+            DAL.Produtos dalProdutos = new DAL.Produtos();
+            return dalProdutos.Select();
+        }
+
+        public Modelo.Produtos SelectById(int id)
+        {
+            DAL.Produtos dalProdutos = new DAL.Produtos();
+            return dalProdutos.SelectById(id);
+        }
+
+        public void Insert(Modelo.Produtos produtos)
+        {
+            DAL.Produtos dalProdutos = new DAL.Produtos();
+            dalProdutos.Insert(produtos);
+        }
+
+        public void Update(Modelo.Produtos produtos)
+        {
+            DAL.Produtos dalProdutos = new DAL.Produtos();
+            dalProdutos.Update(produtos);
+        }
+
+        public void Delete(Modelo.Produtos produtos)
+        {
+            DAL.Produtos dalProdutos = new DAL.Produtos();
+            dalProdutos.Delete(produtos);
+        }
     }
 }
--- SGLJ/SGLJ/Camadas/DAL/Produtos.cs
+++ SGLJ/SGLJ/Camadas/DAL/Produtos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,143 @@ namespace SGLJ.Camadas.DAL
 {
     class Produtos
     {
+        private string strCon = Conexao.getConexao();
+
+        public List<Modelo.Produtos> Select()
+        {
+            List<Modelo.Produtos> lstProdutos= new List<Modelo.Produtos>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Select * from Produtos";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            conexao.Open();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    Modelo.Produtos produtos = new Modelo.Produtos();
+                    produtos.id = Convert.ToInt32(reader["id"]);
+                    produtos.descr = reader["descr"].ToString();
+                    produtos.idTipo_Produto = Convert.ToInt32(reader["idTipo_Produto"]);
+                    produtos.quantidade = Convert.ToInt32(reader["quantidade"]);
+                    produtos.valor = Convert.ToSingle(reader["valor"]);
+                    lstProdutos.Add(produtos);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na Seleção de Produtos...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return lstProdutos;
+        }
+
+        public Modelo.Produtos SelectById(int id)
+        {
+            Modelo.Produtos produtos = new Modelo.Produtos();
+            List<Modelo.Produtos> lstProdutos = new List<Modelo.Produtos>();
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Select * from Produtos where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", id);
+            conexao.Open();
+            try
+            {
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    produtos.id = Convert.ToInt32(reader["id"]);
+                    produtos.descr = reader["descr"].ToString();
+                    produtos.idTipo_Produto = Convert.ToInt32(reader["idTipo_Produto"]);
+                    produtos.quantidade = Convert.ToInt32(reader["quantidade"]);
+                    produtos.valor = Convert.ToSingle(reader["valor"]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na Seleção de Produtos por ID...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return produtos;
+        }
+
+        public void Insert(Modelo.Produtos produtos)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Insert into Produtos values (@idTipo_Produto, @descr, @quantidade, @valor)";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@idTipo_Produto", produtos.idTipo_Produto);
+            cmd.Parameters.AddWithValue("@descr", produtos.descr);
+            cmd.Parameters.AddWithValue("@quantidade", produtos.quantidade);
+            cmd.Parameters.AddWithValue("@valor", produtos.valor);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Deu erro na inserção de Produtos...");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Update(Modelo.Produtos produtos)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Update Produtos set descr=@descr, quantidade=@quantidade, valor=@valor where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@idTipo_Produto", produtos.idTipo_Produto);
+            cmd.Parameters.AddWithValue("@descr", produtos.descr);
+            cmd.Parameters.AddWithValue("@quantidade", produtos.quantidade);
+            cmd.Parameters.AddWithValue("@valor", produtos.valor);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na atualização de Produtos");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        public void Delete(Modelo.Produtos produtos)
+        {
+            SqlConnection conexao = new SqlConnection(strCon);
+            string sql = "Delete from Produtos where id=@id;";
+            SqlCommand cmd = new SqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@id", produtos.id);
+            conexao.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Erro na Remoção de Produtos");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+
     }
 }
